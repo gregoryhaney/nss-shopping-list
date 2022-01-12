2d6a2cdc973bac45c2d1ce0b323225acc32be4d4
Gregory Haney <gregory.haney@gmail.com>
2022-01-12 08:47:19 -0600

    INITIAL: initial commit of creating an array with multiple objects, each with three keys and associated values

---
+++ shopping.js
@@ -0,0 +1,52 @@
+
+
+// create an array holding grocery items as the objects
+// each object will keys of: id, name, price with appropriate values
+// log to the console all the objects in the array
+
+
+const highPriorityItemsArray = [
+    {
+        id: 1,          // number (integer)
+        name: "Milk",   // string
+        price: 4.49     // number (float)
+    },
+
+    {
+        id: 2,          // number (integer)
+        name: "Eggs",   // string
+        price: 3.69     // number (float)
+    },
+
+    {
+        id: 3,                 // number (integer)
+        name: "Pizza Rolls",   // string
+        price: 12.19           // number (float)
+    },
+
+    {
+        id: 4,                    // number (integer)
+        name: "Chicken Breast",   // string
+        price: 11.39              // number (float)
+    },
+
+    {
+        id: 5,            // number (integer)
+        name: "Banana",   // string
+        price: 0.49       // number (float)
+    },
+
+    {
+        id: 6,          // number (integer)
+        name: "Buns",   // string
+        price: 4.69     // number (float)
+    },
+
+    {
+        id: 7,             // number (integer)
+        name: "Ketchup",   // string
+        price: 1.79        // number (float)
+    }
+]
+
+console.log(highPriorityItemsArray)
